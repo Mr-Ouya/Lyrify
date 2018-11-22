b624456763a4e167664142c4352e656384d1d7cb
dakshmalhotra <dakshmalhotra@hotmail.com>
2018-11-22 15:24:08 -0500

    search artist and search track

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
-$("#search-artist").on('click',function(event){
+$("#search-track").on('click',function(event){
     event.preventDefault();
-    var nameinput=$('#artist-name').val();
-    var token='BQAIo9wJUjytnEBc9Krzx4sc6CBe_LlFT2FG8ODtSP2JDnDbbhj-3-r8dzQAZlfluwbK_r7W7OYDXiFublZH74V68qKTKUvN1dOUkvu4iYjRLsyBQJpP7alqKF6DetGiRxnNfcnXOP87XweL4bFPGw';
+    var nameinput=$('#track-name').val();
+    var token='BQDp-_YQ0EYYwgrD9s-O2Mbu3aSOicrItWXtywqtXV1k0pwAcJAJlOE466UIRj-0LhQeNRvENqdOYHyvnwa6mcfrOBGh5XutXOhNl-Kd4ZwBarAsEAn0914iYUbBQrDT94OV2JyWsGaB8eWYCWVelw';
     var queryURL="https://api.spotify.com/v1/search?q="+nameinput+"&type=track&limit=1";
     console.log(nameinput);
     $.ajax({
@@ -11,9 +11,24 @@ $("#search-artist").on('click',function(event){
          },
         method:'GET'
     }).then(function(response){
-    console.log(response)
-    //$("#all-display").html("<a href="+response.tracks.items[0].preview_url+">"+nameinput+"</a>");
+    $("#all-display").html("<a href="+response.tracks.items[0].preview_url+">"+nameinput+"</a>");
     })
     })
 
+    $("#search-artist").on('click',function(event){
+        event.preventDefault();
+        var nameinput=$('#artist-name').val();
+        var token='BQDp-_YQ0EYYwgrD9s-O2Mbu3aSOicrItWXtywqtXV1k0pwAcJAJlOE466UIRj-0LhQeNRvENqdOYHyvnwa6mcfrOBGh5XutXOhNl-Kd4ZwBarAsEAn0914iYUbBQrDT94OV2JyWsGaB8eWYCWVelw';
+        var queryURL="https://api.spotify.com/v1/search?q="+nameinput+"&type=artist&limit=1";
+        console.log(nameinput);
+        $.ajax({
+            url:queryURL,
+            headers: {
+               Authorization: 'Bearer ' + token
+             },
+            method:'GET'
+        }).then(function(response){
+        $("#all-display").html("<a href="+response.artists.items[0].external_urls.spotify+">"+nameinput+"</a>");
+        })
+        })
     
