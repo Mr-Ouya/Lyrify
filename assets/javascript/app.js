caf884d636466fd70cbe4168215930448065b4b1
Feilong Huang <felixh6888@gmail.com>
2018-11-24 10:13:51 -0500

    get album,artist and poster

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $("#submit").on('click',function(event){
 event.preventDefault();
  var nameinput=$('#name-input').val();
  //token is like an API key
-var token='BQChPfT7IHjX7s7X9DgtshGD4DRWKL8HGfZ6KdwYPTzlMqa36Xamuf9BpfS2t_xLIv3E5WZtS9sj2eRsLqrMG4Z7_0nHGV7_-4WQ9YrCobFggZL-s8zWtf9iI3RSAWDp-KT-vRYExmn7e7Qb7Pj8B0a3mtK7C22_nJ2H-g';
+var token='BQBosrmUAIijK78zJIC8vInHkizXRVpTP-0gtxYTs-H1OmTfn41ucS7G0n0QQMMWstmubijWg-uu4lzMFlCFiz32bY8Eki1dsso-S7q2jTBE-dtLemsz7Ks-eipf-zsCNBXYo08FAbe-blpbHF2bwxMK8WRUZ9VWSjhCFQ';
  //queryURL which can get the track
 var queryURL="https://api.spotify.com/v1/search?q="+nameinput+"&type=track&limit=1";
  console.log(nameinput);
@@ -15,8 +15,8 @@ var queryURL="https://api.spotify.com/v1/search?q="+nameinput+"&type=track&limit
  }).then(function(response){
 var result=response.tracks.items[0];
 //visualize the album,artist and image information
-$('#album').text('album name'+result.album.name);
-$('#artist').text(result.artists[0].name);
+$('#album').text('Album Name: '+result.album.name);
+$('#artist').text('Artist Name: '+result.artists[0].name);
 var albumimage=$('<img>');
 albumimage.attr('src',result.album.images[0].url);
 console.log(albumimage);
