a94dfbcba7e32b069a3715ceb29c4179a5a71b0d
Jetender <g.jetender@gmail>
2018-11-24 11:06:37 -0500

    cahnge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,51 +11,108 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var toptopclicks = "";
+var topclicks = [];
+rndarray = [2, 7, 4, 7, 3, 2, 2, 6, 7, 8, 5, 4, 3];
+
 
 
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
-    console.log("hi");
+
     var search = $("#search").val().trim();
-    var clicks = 1;
 
 
-    database.ref(search).push({
+    var onclicks = 1;
 
-        search: search,
-
-        clicks: clicks
-    })
-
-
-    database.ref(search).update({
-
-        clicks: clicks++
-
-    })
+    database.ref().once("value", function (snap) {
+        object = snap.val();
+        console.log("i");
 
 
-});
+        if (snap.hasChild(search)) {
 
+            var child = (object[search].clicks)
+            console.log(child);
+            console.log("hi")
 
+            database.ref(search).child("clicks").transaction(function (currentClicks) {
 
-
-
-database.ref().on("child_added", function (snapshot) {
-
-
-
-            database.ref().on("child_added", function (snapshot) {
-
-                var object = snapshot.val();
-
-                snapshot.forEach(function (snapchild) {
-                    console.log(snapchild.val());
-
-                });
-
-
+                return (currentClicks || 0) + 1
 
 
             })
+
+        } else {
+            console.log("na")
+
+            database.ref(search).set({
+
+
+                search: search,
+                clicks: onclicks
+            })
+
+        }
+
+
+
+    })
+
+})
+
+database.ref().on("value", function (snapshot) {
+
+    snapshot.forEach(function (snap) {
+        object = snap.val();
+        toptopclicks = topclicks.concat([object.clicks, object.search])
+        var array = toptopclicks.sort(function (a, b) {
+            return a[0] - b[0];
+        });
+
+        console.log(array);
+    });
+})
+
+
+function sort(a, b) {
+
+    return a[0] - b[0];
+
+}
+console.log(toptopclicks)
+
+$(".check").html(topclicks[0]);
+
+
+
+var proper = rndarray.sort(function (a, b) {
+    return a - b;
+});
+console.log(proper);
+// sort by name
+
+/* snapshot.forEach(function (childsnap) {
+
+
+
+     var child = childsnap.val();
+
+     console.log(child);
+
+
+
+ })*/
+
+
+
+/*snap.forEach(function (childsnap) {
+
+            objectsnap = childsnap.val();
+            if (search === objectsnap.search) {
+
+            } else {
+
+            }
+        })*/
