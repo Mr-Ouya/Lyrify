5d888396a243e6a527b187bf0fa5a3770d94e284
Just Nick <nicholas.babington@hotmail.com>
2018-11-26 19:29:35 -0500

    all 3 apis working.....real UGLY tho bro

--- assets/javascript/youTube.js
+++ assets/javascript/youTube.js
@@ -1,4 +1,5 @@
 //Youtube API functionality
+
 $(document).ready(function () { 
 	// This function gets the data from the YouTube API and displays it on the page
 	function getResults(searchTerm) {
@@ -14,7 +15,8 @@ $(document).ready(function () {
 					alert("No results!");
 				}
 				// If no results, empty the list
-				displayResults(data.items);
+        displayResults(data.items);
+        console.log(data);
 			}
 		);
 	}
@@ -27,36 +29,39 @@ $(document).ready(function () {
 			//console.log(video.snippet.title);
 			//console.log(video.snippet.thumbnails.high.url);
 			html = html + "<li><p class='line-clamp'>" + video.snippet.title +
-				"</p><a target='_blank' href='https://www.youtube.com/watch?v=" + video.id.videoId + "'><img src='" +  video.snippet.thumbnails.high.url + "'/></a></li>" ;
+				"</p><a target='_blank' href='https://www.youtube.com/watch?v=" + video.id.videoId + "'><img src='" +  video.snippet.thumbnails.default.url + "' width='300' height='300'/></a></li>" ;
 		});
 		$("#music-video").html(html);
 	}
 
 	//Use search track
-	$("#search-track").on('click',function(event){
+	$(".submit-button").on('click',function(event){
     event.preventDefault();
     var sum=$("#track-name").val()+ " " + $("#artist-name").val();
     getResults(sum);
     console.log(sum);
-	});
+  });
+  /*
 	//Use search artist
 	$("#search-artist").on('click',function(event){
 		event.preventDefault();
-		getResults($("#artist-name").val());
-	});
+		var sum=$("#track-name").val()+ " " + $("#artist-name").val();
+    getResults(sum);
+  });
+  */
 });
-        
+       
 
 
 //FUCK TOKENS
 
 //SPOTIFY API
 
-/*
+
 $("#search-track").on('click',function(event){
     event.preventDefault();
     var nameinput=$('#track-name').val();
-    var token='BQC6VcDRC18DRjrNOF7TMzNkOq3vjn-MnfaqGnGI1G0X1GuHhyXjJoH0A1i7XN_q572oHpZr4A69FJk4QU09KKabM0W7BhaKcsuGXCRXo3_FtFfEJBtbCLs2vCu9sVlje0SjcyqXNtqxov3ScWLfPDv0PSxWw9CNjMWkDAoR5od0tNO_STtE_gmFHOcj';
+    var token='BQCeTW5y0p0VPTMAY5k3ek-Ll33gAd4pVOaBi8D7CwGHRR--baxvW7NAB5xZ2NGgkfqM0Qec7iEADpt4573TjprlhePMsys_mEK6eZA3BUuZjcl4Kgr46uRTNxcAQIwV-C70bMGwuuLyk0aBGEHnl7sdwIVRnPH8yZCD87w80UdfcOvcgeca6o1Y2d1x';
     var queryURL="https://api.spotify.com/v1/search?q="+nameinput+"&type=track&limit=1";
     //console.log(nameinput);
     $.ajax({
@@ -97,7 +102,7 @@ $("#search-track").on('click',function(event){
             //console.log(response)
         })
         })
-        */
+        
 
 
         //Lyrics API
@@ -132,7 +137,7 @@ $("#search-track").on('click',function(event){
           $(watchSubmit);
           */
           
-
+          
           $('.submit-button').on('click', function() {
             var artist = $('#artist-name').val();
             var song = $('#track-name').val();
@@ -149,3 +154,4 @@ $("#search-track").on('click',function(event){
                 });
             });
           });
+          
